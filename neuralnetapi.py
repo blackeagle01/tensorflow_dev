ed139686f352fe01ff8e455821b4abb15ca41130
blackeagle01 <tiku.harsh@gmail.com>
2017-10-04 20:43:04 +0530

    api added

--- neuralnetapi.py
+++ neuralnetapi.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
 import numpy as np
+import time
+from sklearn.datasets import load_iris
+from sklearn.cross_validation import train_test_split
+from keras.utils import to_categorical
+
 
 class NeuralNet(object):
+
+
 	def __init__(self,input_dim):
 		self.input_dim=input_dim
 		self.currentstate=None
@@ -13,36 +20,71 @@ class NeuralNet(object):
 
 
 	def addDenseLayer(self,size,activation):
-		w=tf.Variable(np.random.randn(self.currentsize,size ),dtype=tf.float32)
-		b=tf.Variable(np.random.random(size),dtype=tf.float32)
+		w=tf.Variable(np.random.randn(self.currentsize,size ),dtype=tf.float32,name='synapses')
+		b=tf.Variable(np.random.random(size),dtype=tf.float32,name='biases')
 		self.currentsize=size
 		self.synapses.append(w)
 		self.biases.append(b)
 		self.activation.append(activation)
 
 
+
+
+
 	def predict(self,inp):
-		currentstate=tf.Variable(inp,dtype=tf.float32)
+		sess=tf.Session()
+		currentstate=tf.Variable(inp,dtype=tf.float32,name='currentstate')
 		for w,b,a in zip(self.synapses,self.biases,self.activation):
 			currentstate=tf.matmul(currentstate,w)+b
 			if a=='relu':
 				currentstate=tf.nn.relu(currentstate)
 			else:
 				currentstate=tf.nn.softmax(currentstate)
-
-		return tf.Variable(currentstate,dtype=tf.float32)
-
-
-
-sess=tf.Session()
-init=tf.global_variables_initializer()
-sess=tf.Session()
-#init=tf.global_variables_initializer()
-nn=NeuralNet(3)
-nn.addDenseLayer(5,activation='relu')
-nn.addDenseLayer(5,activation='relu')
-nn.addDenseLayer(3,activation='softmax')
+		sess.run(tf.global_variables_initializer())
+		return sess.run(currentstate)
 
 
 
-print(sess.run(nn.predict([[1,3,2]])))
+
+	def fit(self,inputdata,outputs):
+		sess=tf.Session()
+		currentstate=tf.Variable(inputdata,dtype=tf.float32,name='currentstate')
+		for i in range(len(self.synapses)):
+			currentstate=tf.matmul(currentstate,self.synapses[i])+self.biases[i]
+			if self.activation[i]=='relu':
+				currentstate=tf.nn.relu(currentstate)
+			else:
+				pass
+
+		loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=currentstate,labels=outputs))
+		train=tf.train.AdamOptimizer(0.005).minimize(loss)
+		sess=tf.Session()
+		sess.run(tf.global_variables_initializer())
+		for i in range(1000):
+			#print("Epoch ", i+1)
+			sess.run(train)
+			#print("Loss : ",sess.run(loss))
+			#time.sleep(0.01)
+
+
+
+
+if __name__=="__main__":
+
+	d=load_iris()
+	X_train,X_test,Y_train,Y_test=train_test_split(d.data,d.target)
+	Y_train=to_categorical(Y_train)
+	Y_test=to_categorical(Y_test)
+
+	sess=tf.Session()
+	init=tf.global_variables_initializer()
+	sess.run(init)
+	nn=NeuralNet(4)
+	nn.addDenseLayer(6,activation='relu')
+	nn.addDenseLayer(6,activation='relu')
+	nn.addDenseLayer(3,activation='softmax')
+	nn.fit(X_train,Y_train)
+	pred=nn.predict(X_train[:4])
+	sess.run(init)
+	print(np.round(pred),Y_train[:4])
+
